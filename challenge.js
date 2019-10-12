2f3c1c5d3a59d796727325393b38ce72b1ef268d
AlwinaO <aoyewole2586@gmail.com>
2019-10-12 18:58:34 -0400

    Done.

---
+++ challenge.js
@@ -0,0 +1,9 @@
+let domChallenge = setInterval(() => {
+//    if page has loaded, then increment counter
+    document.addEventListener('DOMContentLoaded', (event) => {
+        let counter = 0;
+        while (counter < 100) {
+            counter++
+        }
+    })
+}, 1000);
